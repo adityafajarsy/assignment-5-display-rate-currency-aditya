e1ccdb847656fd6da215e783ee4c9986463e9c08
adityafajarsy <adityafajar.sy90@gmail.com>
2024-11-17 11:13:45 +0700

    menambahkan file .env.example

--- src/App.jsx
+++ src/App.jsx
@@ -5,89 +5,13 @@ import './App.css'
 import CardName from './components/CardName'
 import ButtonChange from './components/ButtonChange'
 
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-class AppClass extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      name : "John Doe"
-    }
-}
-
-  changeName = () => {
-    this.setState({
-     name: "Jane Doe" 
-    })
-  }
-
-
-  componentDidMount() {
-    console.log("component did mount");
-  }
-
-  componentDidUpdate(_, prevState) {
-    if(prevState.name !== this.state.name) {
-    console.log("component did update")
-  }
-}
-
-  shouldComponentUpdate(_, nextState) {
-    if (nextState.name === "Jane Doe") {
-      console.log("component should update")
-      return false 
-    }
-    return true
-  }
-
-  render() {
-    return (
-      <div>
-        <p>ini Paragraf</p>
-        <h1>Hello {this.state.name}!</h1>
-        <CardName name={this.state.name}></CardName>
-        <ButtonChange changeEventListener={this.changeName}></ButtonChange>
-        {/* <button onClick={this.changeName}>Click Me!</button> */}
-        
-      </div>
-    );
-  }
-}
-
-
-function App() {
+export default function App() {
   const [data, setData] = useState([])
   const apiKey = import.meta.env.VITE_API_KEY;
+  const apiURL = import.meta.env.VITE_API_URL;
   
   useEffect(() => {
-    fetch(`https://api.currencyfreaks.com/v2.0/rates/latest?apikey=${apiKey}`)
+    fetch(apiURL)
     .then(response => response.json())
     .then(result => {
       console.log(result.rates)
@@ -162,4 +86,81 @@ return (
 }
 
 
-export default App
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// class AppClass extends React.Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       name : "John Doe"
+//     }
+// }
+
+//   changeName = () => {
+//     this.setState({
+//      name: "Jane Doe" 
+//     })
+//   }
+
+
+//   componentDidMount() {
+//     console.log("component did mount");
+//   }
+
+//   componentDidUpdate(_, prevState) {
+//     if(prevState.name !== this.state.name) {
+//     console.log("component did update")
+//   }
+// }
+
+//   shouldComponentUpdate(_, nextState) {
+//     if (nextState.name === "Jane Doe") {
+//       console.log("component should update")
+//       return false 
+//     }
+//     return true
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <p>ini Paragraf</p>
+//         <h1>Hello {this.state.name}!</h1>
+//         <CardName name={this.state.name}></CardName>
+//         <ButtonChange changeEventListener={this.changeName}></ButtonChange>
+//         {/* <button onClick={this.changeName}>Click Me!</button> */}
+        
+//       </div>
+//     );
+//   }
+// }
+
+
